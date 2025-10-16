c3414ef9066c9de58641ebba57c34209f8cd42c4
Caroline Yanka <caroline.ferreira@live.vu.edu.au>
2025-10-16 13:10:49 +1100

    DashboardRepository.kt

--- app/src/main/java/com/example/s8132684assignment2/repository/DashboardRepository.kt
+++ app/src/main/java/com/example/s8132684assignment2/repository/DashboardRepository.kt
@@ -1,4 +1,12 @@
 package com.example.s8132684assignment2.repository
 
-class DashboardRepository {
+import com.example.s8132684assignment2.data.DashboardResponse
+import com.example.s8132684assignment2.network.ApiService
+import jakarta.inject.Inject
+
+class DashboardRepository @Inject constructor(private val apiService: ApiService) {
+
+    suspend fun getDashboard(keypass: String): DashboardResponse {
+        return apiService.getDashboard(keypass)
+    }
 }
